a8ad213e95550e60184e2c0fe2898ee745d95e3d
Peter Springer <peterspringer829@gmail.com>
2017-02-14 16:58:53 -0700

    initial setup

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+
+module.exports = {
+  entry: {
+    main: ['babel-polyfill', './lib/index.js']
+  },
+  output: {
+    path: __dirname,
+    filename: '[name].bundle.js',
+  },
+  module: {
+    loaders: [
+      { test: /\.css$/, loader: 'style!css' },
+      { test: /\.scss$/, loader: 'style!css!resolve-url!sass?sourceMap' },
+      { test: /\.png$/, loader: 'url-loader', query: { mimetype: 'image/png'} },
+      { test: /\.jpg$/, loader: 'url-loader', query: { mimetype: 'image/jpg'} },
+      { test: /\.js?$/, exclude: /(node_modules|bower_components)/,
+        loader: 'babel',
+        query: {
+          presets: ['es2015', 'react'],
+        },
+      },
+    ]
+  },
+  resolve: {
+    extensions: ['', '.js', '.json', '.jsx', '.scss', '.css']
+  }
+};
